d4a88d71a05eddb7a0186a3f6ca107cd00e63e32
Jonathon Ball <jonathon.ball@gmail.com>
2018-10-02 23:57:19 -0400

    updated index to use actions

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-const luxafor = require('node-luxafor2')();
+const {Luxafor} = require('node-luxafor2');
+const luxafor = new Luxafor();
 const tinycolor = require('tinycolor2');
 const yargs = require('./lib/yargs');
-
-//const isPresetColor = property => property in luxafor.API.COLOR;
-
-const getPresetColor = function(color) {
-    color = color.toUpperCase();
-    if (! color in luxafor.API.COLOR) {
-        console.error('Invalid preset color.  Choose from:');
-        console.error(Object.keys(luxafor.API.COLOR).join(', '));
-        process.exit(1);        
-    }
-    return luxafor.API.COLOR[color];
-}
-
-const getLed = function(led) {
-    led = led.toUpperCase();
-    if (! led in luxafor.API.LED) {
-        console.error('Invalid LED selection.  Choose from:');
-        console.error(Object.keys(luxafor.API.LED).join(', '));
-        process.exit(1);
-    }
-    return luxafor.API.LED[led];
-}
+const Actions = require('./lib/actions');
 
 luxafor.init();
+var Action = null;
 
 if (yargs.preset) {
-    luxafor.simpleColor(getPresetColor(yargs.preset));
+    Action = new Actions.Preset();
 } else if (yargs.strobe) {
-    luxafor.strobe(
-        getLed(yargs.led),
-        yargs.red,
-        yargs.green,
-        yargs.blue,
-        yargs.time,
-        yargs.repeat
-    );
+    Action = new Actions.Strobe();
 }
+Action.go(luxafor, yargs);
