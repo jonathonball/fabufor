1d8a18e957d55e89425cc21605c03382d581ae01
Jonathon Ball <jonathon.ball@gmail.com>
2018-10-03 20:38:31 -0400

    added wave function and sorted out some issues with colors

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const {Luxafor} = require('node-luxafor2');
 const luxafor = new Luxafor();
 const yargs = require('./lib/yargs');
 const Actions = require('./lib/actions');
+const tinycolor = require('tinycolor2');
+
+let userColor = yargs.color;
+if (userColor) {
+    userColor = tinycolor(yargs.color).toRgb();
+    yargs.red = yargs.r = userColor.r;
+    yargs.green = yargs.g = userColor.g;
+    yargs.blue = yargs.b = userColor.b;
+}
+
+if (yargs.color && Actions.Actions.isBlack(yargs)) {
+    console.warn("Warning: You specified a color but the result was black.");
+}
+if (yargs.red === undefined || yargs.green === undefined || yargs.blue === undefined) {
+    Actions.Actions.notifyExit('No color provided.');
+}
 
 luxafor.init();
 var Action = null;
@@ -10,5 +26,12 @@ if (yargs.preset) {
     Action = new Actions.Preset();
 } else if (yargs.strobe) {
     Action = new Actions.Strobe();
+} else if (yargs.wave) {
+    Action = new Actions.Wave();
 }
-Action.go(luxafor, yargs);
+
+if (Action) {
+    Action.go(luxafor, yargs);
+} else {
+    Actions.notifyExit('No action was specified.');
+}
